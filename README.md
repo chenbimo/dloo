99000b89cbf7933c26a452ca372296179e9cbe3c
陈随易 <chenbimo@gmail.com>
2024-11-29 15:38:29 +0800

    减少依赖，完善文档

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-# dloo - 陈随易官方 NPM 项目模板下载工具
+# dloo - NPM 包下载工具
 
 作者：[陈随易 https://chensuiyi.me](https://chensuiyi.me)
-
-### 用途
-
-下载 `npm` 项目模板到当前目录的 `dloo-package` 目录下。
 
 ### 使用
 
@@ -22,39 +18,12 @@ pnpm dlx dloo -t yibase
 
 ### 指令
 
--   `-t 模板名称`
--   `-v 版本`，默认值 latest
+-   `-t npm包名称`
+-   `-v 版本`，默认值 `latest`
 
 ### 注意
 
-下载后请将 `.dloo` 中的文件移至目标目录下进行开发。
-
-### 模板
-
-```javascript
-const templateLists = [
-    {
-        name: 'funpi 接口项目开发模板',
-        value: 'funpiba'
-    },
-    {
-        name: 'yibase 基础项目开发模板',
-        value: 'yibase'
-    },
-    {
-        name: 'yiadmin 后台项目开发模板',
-        value: 'yiadmin'
-    },
-    {
-        name: 'yiuni 小程序项目开发模板',
-        value: 'yiuni'
-    },
-    {
-        name: 'yidocs 易文档基础模板',
-        value: 'yidocs'
-    }
-];
-```
+下载后请将 `.dloo` 中的所有文件移至目标目录下。
 
 ### 赞赏作者
 
--- dloo.js
+++ dloo.js
@@ -2,53 +2,21 @@
 import fs from 'node:fs';
 import pacote from 'pacote';
 import { cli4state } from 'cli4state';
-import { minimist } from './minimist.js';
-
-// 模板列表
-const templateLists = [
-    {
-        name: 'funpiba 接口项目开发模板',
-        value: 'funpiba'
-    },
-    {
-        name: 'yibase 基础项目开发模板',
-        value: 'yibase'
-    },
-    {
-        name: 'yiadmin 后台项目开发模板',
-        value: 'yiadmin'
-    },
-    {
-        name: 'yiuni 小程序项目开发模板',
-        value: 'yiuni'
-    },
-    {
-        name: 'yidocs 易文档基础模板',
-        value: 'yidocs'
-    }
-];
-const templateNames = templateLists.map((item) => item.value);
+import { minimist } from './utils/minimist.js';
+import { log4state } from './utils/index.js';
 const registry = 'https://registry.npmmirror.com';
-const dd = 1;
 
 async function main() {
-    console.log(cli4state.info, '开发者：陈随易（https://chensuiyi.me）');
-    console.log('-----------------------------------------');
-
     try {
         const options = minimist(process.argv.slice(2));
-        const teimplateInfo = templateLists.find((item) => item.value === options.t);
-        if (!teimplateInfo) {
-            console.log(cli4state.error, `模板名称错误，请指定 ${templateNames} 模板之一`);
-        }
         const version = options.v ? options.v : 'latest';
-        console.log(cli4state.info, teimplateInfo.name + '下载中...');
+        console.log(log4state('info'), options.t + ' 下载中...');
         const fetchData = await fetch(`${registry}/${options.t}/${version}`);
         const metaData = await fetchData.json();
         const downMeta = await pacote.extract(metaData.dist.tarball, './.dloo', {});
-        console.log(cli4state.success, '资源已下载到默认的 .dloo 目录，请移动到正确的目录!');
+        console.log(log4state('success'), '资源已下载到默认的 [.dloo] 目录，请移动到正确的目录!');
     } catch (err) {
-        console.log(cli4state.error, '资源错误或不存在，请检查版本是否正确');
+        console.log(log4state('error'), '资源错误或不存在，请检查 [名称] 或 [版本] 是否正确');
     }
 }
 
